f4f8185453e3b317dfc350de854dba95146f1d55
Walter Setti <walter.setti@iit.it>
2021-09-30 17:30:45 +0200

    Ground_Zero1

---
+++ 4weeks4inclusion/4weeks4inclusion/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.Threading;
+
+namespace _4weeks4inclusion
+{
+    public partial class Main : Form
+    {
+        public static readonly string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+        public static readonly string resourcesPath = Path.GetDirectoryName(Application.ExecutablePath) + "\\resources";
+        private const string background_image = "galaxy.jpg";
+        private UserControl currUC = null;
+        public int step;
+        public SoundPlayer player = null;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
